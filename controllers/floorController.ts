1d38fe8f38188a675b4efb80be997694f79e8daf
Duncan G <dgatuthu@gmail.com>
2022-04-17 15:25:23 +0300

    Created the floor controller

---
+++ controllers/floorController.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createFloor = async (req: Request, res: Response) => {
+  const { name, house_count, rent } = req.body;
+
+  const newFloor = await prisma.floor.create({
+    data: {
+      name,
+      house_count: Number(house_count),
+      rent: Number(rent),
+    },
+  });
+
+  res.json(newFloor);
+};
+
+const getFloors = async (req: Request, res: Response) => {
+  const floors = await prisma.floor.findMany();
+  res.json(floors);
+};
+
+const updateFloor = async (req: Request, res: Response) => {
+  const { floor_no, name, house_count } = req.body;
+  const updatedFloor = await prisma.floor.update({
+    where: {
+      floor_no: Number(floor_no),
+    },
+    data: {
+      name,
+      house_count: Number(house_count),
+    },
+  });
+};
+
+const deleteFloor = async (req: Request, res: Response) => {
+  const floor_no = req.params.id;
+  const deletedFloor = await prisma.floor.delete({
+    where: {
+      floor_no: Number(floor_no),
+    },
+  });
+
+  res.json(deletedFloor);
+};
+
+export { createFloor, getFloors, updateFloor, deleteFloor };
--- routes/floorRoutes.ts
+++ routes/floorRoutes.ts
@@ -1,53 +1,22 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import {
+  createFloor,
+  getFloors,
+  updateFloor,
+  deleteFloor,
+} from "../controllers/floorController";
 
 const prisma = new PrismaClient();
 
 const floorRoute = express.Router();
 
-floorRoute.post("/floors", async (req: Request, res: Response) => {
-  const { name, house_count, rent } = req.body;
+floorRoute.post("/floors", createFloor);
 
-  const newFloor = await prisma.floor.create({
-    data: {
-      name,
-      house_count: Number(house_count),
-      rent: Number(rent),
-    },
-  });
+floorRoute.get("/floors", getFloors);
 
-  res.json(newFloor);
-});
+floorRoute.put("/floors", updateFloor);
 
-floorRoute.get("/floors", async (req: Request, res: Response) => {
-  const floors = await prisma.floor.findMany();
-  res.json(floors);
-});
-
-floorRoute.put("/floors", async (req: Request, res: Response) => {
-  const { floor_no, name, house_count } = req.body;
-  const updatedFloor = await prisma.floor.update({
-    where: {
-      floor_no: Number(floor_no),
-    },
-    data: {
-      name,
-      house_count: Number(house_count),
-    },
-  });
-
-  res.json(updatedFloor);
-});
-
-floorRoute.delete("/floors/:id", async (req: Request, res: Response) => {
-  const floor_no = req.params.id;
-  const deletedFloor = await prisma.floor.delete({
-    where: {
-      floor_no: Number(floor_no),
-    },
-  });
-
-  res.json(deletedFloor);
-});
+floorRoute.delete("/floors/:id", deleteFloor);
 
 export default floorRoute;
