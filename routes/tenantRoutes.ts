8574447445ae1f960a9189518bf593939f029d9c
Duncan G <dgatuthu@gmail.com>
2022-04-17 15:43:01 +0300

    Created the Tenants controller

--- routes/tenantRoutes.ts
+++ routes/tenantRoutes.ts
@@ -1,55 +1,22 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import {
+  createTenant,
+  updateTenants,
+  getTenants,
+  deleteTenant,
+} from "../controllers/tenantController";
 
 const prisma = new PrismaClient();
 
 const tenantRoute = express.Router();
 
-tenantRoute.post("/tenants", async (req: Request, res: Response) => {
-  const { name, email, phone, passport_id, house_no, floor_no, status } =
-    req.body;
-  const newTenant = await prisma.tenant.create({
-    data: {
-      name,
-      email,
-      phone: Number(phone),
-      passport_id: Number(passport_id),
-      floor_no: Number(floor_no),
-      status,
-    },
-  });
+tenantRoute.post("/tenants", createTenant);
 
-  res.json(newTenant);
-});
+tenantRoute.get("/tenants", getTenants);
 
-tenantRoute.get("/tenants", async (req: Request, res: Response) => {
-  const allTenants = await prisma.tenant.findMany();
-  res.json(allTenants);
-});
+tenantRoute.put("/tenants", updateTenants);
 
-tenantRoute.put("/tenants", async (req: Request, res: Response) => {
-  const { id, email } = req.body;
-
-  const updatedTenant = await prisma.tenant.update({
-    where: {
-      id: Number(id),
-    },
-    data: {
-      email,
-    },
-  });
-  res.json(updatedTenant);
-});
-
-tenantRoute.delete("/tenants/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const deletedTenant = await prisma.tenant.delete({
-    where: {
-      id: Number(id),
-    },
-  });
-
-  res.json(deletedTenant);
-});
+tenantRoute.delete("/tenants/:id", deleteTenant);
 
 export default tenantRoute;
