dc5aa83b53ce69f8bce39e06c37f985e193f4593
Duncan G <dgatuthu@gmail.com>
2022-04-17 12:19:36 +0300

    Created the Update and Delete floor API

--- index.ts
+++ index.ts
@@ -22,5 +22,31 @@ app.get("/floors", async (req: Request, res: Response) => {
   res.json(floors);
 });
 
+app.put("/floors", async (req: Request, res: Response) => {
+  const { floor_no, name, house_count } = req.body;
+  const updatedFloor = await prisma.floor.update({
+    where: {
+      floor_no: Number(floor_no),
+    },
+    data: {
+      name,
+      house_count: Number(house_count),
+    },
+  });
+
+  res.json(updatedFloor);
+});
+
+app.delete("/floors/:id", async (req: Request, res: Response) => {
+  const floor_no = req.params.id;
+  const deletedFloor = await prisma.floor.delete({
+    where: {
+      floor_no: Number(floor_no),
+    },
+  });
+
+  res.json(deletedFloor);
+});
+
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
