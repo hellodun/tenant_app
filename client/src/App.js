a7b7970694c26e91a831f4bfd89afbd40d95b5b4
Duncan G <dgatuthu@gmail.com>
2022-04-20 15:18:56 +0300

    Initialised he client using react

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Tenant app</h1>
+    </div>
+  );
+}
+
+export default App;
--- index.ts
+++ index.ts
@@ -3,21 +3,12 @@ import buildingRoute from "./routes/buildingRoutes";
 import tenantRoute from "./routes/tenantRoutes";
 
 const app = express();
-app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
-
-app.get("/tenants/add", (req, res) => {
-  res.render("addTenant", { title: "Add tenant" });
-});
-
-app.get("/buildings/add", (req, res) => {
-  res.render("addBuilding", { title: "Add building" });
+  res.redirect("/tenants");
 });
 
 app.use(buildingRoute);
