4647bfb860a6693804a8f10291620e17f499785d
Duncan G <dgatuthu@gmail.com>
2022-04-20 16:53:52 +0300

    Added styling to addTenant and addBuilding

--- client/src/views/AddBuilding.js
+++ client/src/views/AddBuilding.js
@@ -6,15 +6,35 @@ export default function AddBuilding() {
     <>
       <Navbar />
       <div>
-        <h3>Enter the bildings's details</h3>
-        <form action="/buildings" method="post">
-          <label for="name">Enter floor name</label>
-          <input type="text" name="name" id="name" />
-          <label for="count">Enter the number of houses</label>
-          <input type="number" name="house_count" id="house_count" />
-          <label for="rent">Enter the rent for the building</label>
-          <input type="text" name="rent" id="rent" />
-          <input type="submit" />
+        <h3 className='text-2xl font-semibold mx-60 my-10'>Enter the Building details</h3>
+        <form action="/buildings" method="post" className='mx-48'>
+          <div className="font-normal text-md">
+            <div className="flex justify-between mb-3">
+              <label for="name">Enter floor name</label>
+              <input type="text" name="name" id="name" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label for="count">Enter the number of houses</label>
+              <input type="number" name="house_count" id="house_count" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label for="count">Enter the number of houses</label>
+              <input type="number" name="house_count" id="house_count" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label for="count">Enter the number of houses</label>
+              <input type="number" name="house_count" id="house_count" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label for="rent">Enter the building rent</label>
+              <input type="text" name="rent" id="rent" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+            <input type="submit" className="rounded-md shadow-sm text-white font-semibold bg-teal-500 px-4 py-1" />
+          </div>
         </form>
       </div>
     </>
--- client/src/views/AddTenants.js
+++ client/src/views/AddTenants.js
@@ -6,26 +6,51 @@ export default function AddTenants() {
     <>
       <Navbar />
       <div>
-        <h3>Enter new tenant details</h3>
-        <form action="/tenants/add" method="post">
-          <label htmlFor="name">Enter name</label>
-          <input type="text" name="name" id="name" />
-          <label htmlFor="email">Enter email</label>
-          <input type="email" name="email" id="email" />
-          <label htmlFor="phone">Enter phone number</label>
-          <input type="tel" name="phone" id="phone" />
-          <label htmlFor="id">Enter National ID no./Passport no.</label>
-          <input type="number" name="passport_id" id="id" />
-          <label htmlFor="house_no">Enter house number</label>
-          <input type="text" name="house_no" id="house_no" />
-          <label htmlFor="floor">Enter the building</label>
-          <input type="number" name="building_id" id="building_id" />
-          <label htmlFor="status">Select rent status</label>
-          <select name="status" id="status">
-            <option value="pain">Paid</option>
-            <option value="Pending">Pending</option>
-          </select>
-          <input type="submit" />
+        <h3 className='text-2xl font-semibold mx-60 my-10'>Enter new tenant details</h3>
+        <form action="/tenants/add" method="post" className="mx-48">
+          <div className="font-normal text-md">
+            <div className="flex justify-between mb-3" >
+              <label htmlFor="name">Enter name</label>
+              <input type="text" name="name" id="name" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3" >
+              <label htmlFor="email">Enter email</label>
+              <input type="email" name="email" id="email" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label htmlFor="phone">Enter phone number</label>
+              <input type="tel" name="phone" id="phone" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label htmlFor="id">Enter tenant ID no.</label>
+              <input type="number" name="passport_id" id="id" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label htmlFor="house_no">Enter house number</label>
+              <input type="text" name="house_no" id="house_no" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label htmlFor="floor">Enter the building</label>
+              <input type="number" name="building_id" id="building_id" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" />
+            </div>
+
+            <div className="flex justify-between mb-3">
+              <label htmlFor="status">Select rent status</label>
+              <select name="status" id="status" className="bg-gray-100 appearance-none border-2 border-teal-400 rounded px-2 text-gray-700 ml-2 focus:outline-teal-600" >
+                <option value="pain">Paid</option>
+                <option value="Pending">Pending</option>
+              </select>
+            </div>
+            <div>
+
+              <input type="submit" className="rounded-md shadow-sm text-white font-semibold bg-teal-500 px-4 py-1" />
+            </div>
+          </div>
         </form>
       </div>
     </>
